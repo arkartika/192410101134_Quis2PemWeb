de7f4c745f25c0029146fe9b42b611f71d4b1d33
arkartika <71867729+arkartika@users.noreply.github.com>
2021-05-15 15:41:53 +0700

    login php

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+include "koneksi.php";
+
+//$username = mysqli_real_escape_string($connect, $_POST['username']);
+//$password = mysqli_real_escape_string($connect, $_POST['password']);
+//$password = md5($password);
+
+$sql = mysqli_query($connect, "SELECT * FROM data WHERE username= '$_POST[username]' AND password ='$_POST[password]' ");
+$data = mysqli_fetch_array($sql);
+
+if( ! empty($data)){
+    $_SESSION['username'] = $data['username'];
+    $_SESSION['nama'] = $data['nama'];
+
+    setcookie("message", "delete",time()-1);
+
+    header("location: welcome.php");
+}else{
+    setcookie("message", "Maaf, Username atau Password tidak sesuai", time()+3600);
+
+    header("location: index.php");
+}
+?>
